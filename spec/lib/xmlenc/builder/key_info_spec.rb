aca11e27f5be7f45ca982e064bf6c02d0b034d56
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-09-26 12:40:59 +0200

    Removed the encrypted_key fixture and added more specs to the EncryptedKey element.

--- spec/lib/xmlenc/builder/key_info_spec.rb
+++ spec/lib/xmlenc/builder/key_info_spec.rb
@@ -2,23 +2,35 @@ require "spec_helper"
 
 describe Xmlenc::Builder::KeyInfo do
 
-  let(:xml) { File.read File.join("spec", "fixtures", "encrypted_key.xml") }
-  subject   { described_class.parse(xml, single: true) }
+  let(:xml) { File.read File.join("spec", "fixtures", "encrypted_document.xml") }
+  subject   { described_class.parse(xml) }
 
   describe "#parse" do
-    it "should create a KeyInfo" do
-      expect(subject).to be_a Xmlenc::Builder::KeyInfo
+    it "should create two KeyInfo elements" do
+      subject.each do |element|
+        expect(element).to be_a Xmlenc::Builder::KeyInfo
+      end
+
+      expect(subject.size).to eq 2
     end
 
-    it "should parse the key name" do
-      expect(subject.key_name).to eq "my-rsa-key"
+    describe "key name" do
+      it "doesn't have a key name in the first key info element" do
+        expect(subject.first.key_name).to be_nil
+      end
+
+      it "should parse the key name in the second key info element" do
+        expect(subject.last.key_name).to eq "my-rsa-key"
+      end
     end
 
     describe "encrypted key" do
-      let(:xml) { File.read File.join("spec", "fixtures", "encrypted_document.xml") }
+      it "should parse the encrypted key in the first key info element" do
+        expect(subject.first.encrypted_key).to be_a Xmlenc::Builder::EncryptedKey
+      end
 
-      it "should parse the encrypted key" do
-        expect(subject.encrypted_key).to be_a Xmlenc::Builder::EncryptedKey
+      it "doesn't have an encrypted key in the second key info element" do
+        expect(subject.last.encrypted_key).to be_nil
       end
     end
   end
