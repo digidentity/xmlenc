176bf183b6da2c5bf5ac448f14f08f64eb6f4b7e
Johnny Dongelmans <jdongelmans@digidentity.eu>
2013-10-24 14:49:05 +0200

    and more tests

--- spec/lib/xmlenc/encrypted_document_spec.rb
+++ spec/lib/xmlenc/encrypted_document_spec.rb
@@ -11,21 +11,21 @@ describe Xmlenc::EncryptedDocument do
       expect(subject.document).to be_a(Nokogiri::XML::Document)
     end
   end
-  #
-  #describe 'encrypted_keys' do
-  #  it 'returns the encrypted keys' do
-  #    expect(subject.encrypted_keys.count).to be == 1
-  #  end
-  #
-  #  it 'converts the elements to EncryptedKey' do
-  #    all_converted = subject.encrypted_keys.all? { |ek| ek.is_a?(Xmlenc::EncryptedKey) }
-  #    expect(all_converted).to be_true
-  #  end
-  #end
 
-  #describe 'decrypt' do
-  #  it 'replaces the encrypted data with the unencrypted elements' do
-  #    expect(subject.decrypt(private_key).chomp).to be == plain_xml
-  #  end
-  #end
+  describe 'encrypted_keys' do
+    it 'returns the encrypted keys' do
+      expect(subject.encrypted_keys.count).to be == 1
+    end
+
+    it 'converts the elements to EncryptedKey' do
+      all_converted = subject.encrypted_keys.all? { |ek| ek.is_a?(Xmlenc::EncryptedKey) }
+      expect(all_converted).to be_true
+    end
+  end
+
+  describe 'decrypt' do
+    it 'replaces the encrypted data with the unencrypted elements' do
+      expect(subject.decrypt(private_key).chomp).to be == plain_xml
+    end
+  end
 end
