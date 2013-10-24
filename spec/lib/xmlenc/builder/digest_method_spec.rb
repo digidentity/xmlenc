3298ff5579dbcb239c72c1dac3bbf1456ab15184
Johnny Dongelmans <jdongelmans@digidentity.eu>
2013-10-24 13:53:00 +0200

    added tests for new elements

--- spec/lib/xmlenc/builder/digest_method_spec.rb
+++ spec/lib/xmlenc/builder/digest_method_spec.rb
@@ -2,4 +2,17 @@ require "spec_helper"
 
 describe Xmlenc::Builder::DigestMethod do
 
+  let(:xml) { File.read File.join("spec", "fixtures", "template2.xml") }
+  subject { described_class.parse(xml, single: true) }
+
+  describe "#parse" do
+    it "should have one algorithm" do
+      expect(subject.algorithm).to eq "http://www.w3.org/2000/09/xmldsig#sha1"
+    end
+
+    it "raises error when no algorithm" do
+      subject.algorithm = nil
+      expect(subject).to have(1).error_on :algorithm
+    end
+  end
 end
